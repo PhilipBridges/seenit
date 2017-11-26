97fc2a8e3215d61be3ae146ff30e8c3b406297e8
fonzied <you.dunno@gmail.com>
2017-11-26 11:56:38 -0500

    added mui

---
+++ src/components/RaisedButton.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Button from 'material-ui/Button';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+    backgroundColor: '#2196f3',
+    fontSize: '14px',
+    color: 'white',
+    display: 'inline'
+  },
+  input: {
+    display: 'none',
+  },
+});
+
+function RaisedButtons(props) {
+  const { classes } = props;
+  return (
+    <div>
+      <Button raised className={classes.button}>
+        New Post
+      </Button>
+    </div>
+  );
+}
+
+RaisedButtons.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(RaisedButtons);
